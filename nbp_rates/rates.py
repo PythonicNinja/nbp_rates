54a1850d37809630e30bfcc3aa3d2de905be7603
Wojtek Nowak <mail@pythonic.ninja>
2023-03-20 13:44:15 +0100

    handle case of surpassed threshold of 365 days,

--- nbp_rates/rates.py
+++ nbp_rates/rates.py
@@ -9,7 +9,10 @@ def fetch_rates_to_pln_nbp(start_date: datetime, end_date: datetime, currency="E
     end_str = end_date.strftime("%Y-%m-%d")
     url = f"http://api.nbp.pl/api/exchangerates/rates/a/{currency.lower()}/{start_str}/{end_str}/"
     req = requests.get(url)
-    rates = req.json()["rates"]
+    try:
+        rates = req.json()["rates"]
+    except requests.exceptions.JSONDecodeError:
+        raise ValueError(f"NBP API returned invalid JSON: {req.text}")
     rates = [{'date': r['effectiveDate'], 'rate': r['mid']} for r in rates]
     min_rate = min(r['rate'] for r in rates)
     max_rate = max(r['rate'] for r in rates)
