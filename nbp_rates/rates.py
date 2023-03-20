3013ede58b08ac0c584e0418cfbd2ceed16acb52
Wojtek Nowak <mail@pythonic.ninja>
2023-03-20 18:55:51 +0100

    moving avg based price predictor,

--- nbp_rates/rates.py
+++ nbp_rates/rates.py
@@ -20,3 +20,29 @@ def fetch_rates_to_pln_nbp(start_date: datetime, end_date: datetime, currency="E
         r['is_min'] = r['rate'] == min_rate
         r['is_max'] = r['rate'] == max_rate
     return rates
+
+
+def moving_avg(rates: List[Dict], n=3):
+    for i in range(len(rates)):
+        if i < n:
+            continue
+        avg = sum(r['rate'] for r in rates[i-n:i]) / n
+        rates[i][f"avg_{n}"] = avg
+    return rates
+
+
+def predict_price_for_period(start_date: datetime, end_date: datetime, currency="EUR"):
+    rates = fetch_rates_to_pln_nbp(start_date, end_date, currency)
+    rates = moving_avg(rates, n=3)
+    rates = moving_avg(rates, n=7)
+    rates = moving_avg(rates, n=14)
+    rates = moving_avg(rates, n=30)
+    rates = moving_avg(rates, n=90)
+    rates = moving_avg(rates, n=180)
+    rates = moving_avg(rates, n=360)
+    return {k: v for k, v in rates[-1].items() if k.startswith("avg_")}
+
+
+if __name__ == '__main__':
+    moving_averages = predict_price_for_period(datetime.datetime(2022, 3, 20), datetime.datetime(2023, 3, 20))
+    print(moving_averages)
